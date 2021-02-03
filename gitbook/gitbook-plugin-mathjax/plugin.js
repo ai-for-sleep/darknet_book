49d76d03a4aa2fee1753c8a40ede038ef9deef1a
jjeamin <woalsdl500@naver.com>
2021-02-03 16:06:29 +0900

    Update mathjax refresh

--- gitbook/gitbook-plugin-mathjax/plugin.js
+++ gitbook/gitbook-plugin-mathjax/plugin.js
@@ -1,11 +1,11 @@
 require(["gitbook"], function(gitbook) {
+  window.addEventListener('load', function (event) {
     MathJax.Hub.Config({
-        tex2jax: {}
+      tex2jax: {}
     });
-
 
     gitbook.events.bind("page.change", function() {
-        MathJax.Hub.Typeset()
+      MathJax.Hub.Typeset()
     });
+  });
 });
-
